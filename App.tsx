869b077a9464a049312ad657f3ee3ce65e789815
Bui Cong Hieu <buihieu9259@gmail.com>
2025-03-15 21:21:11 +0700

    video 46

--- App.tsx
+++ App.tsx
@@ -1,38 +1,66 @@
 import { useState } from "react";
-import { Keyboard, StyleSheet ,TouchableWithoutFeedback,View, Text} from "react-native";
+import { Keyboard, StyleSheet, TouchableWithoutFeedback, View, Text, Button } from "react-native";
 
 import InputTodo from "./components/todo/input.todo";//RELATIVE PATH
 import ListTodo from "./components/todo/list.todo";
 import FlexBox from "./components/todo/flexbox";
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useNavigation } from "@react-navigation/native";
+export default function App() {
 
-export default function App(){
+  const Stack = createNativeStackNavigator();
 
-    const Stack = createNativeStackNavigator();
-    function HomeScreen() {
-        return (
-          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home Screen</Text>
-          </View>
-        );
-      }
-    
-      
-    function DetailsScreen() {
-        return (
-          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Details Screen</Text>
-          </View>
-        );
-      }
+  function HomeScreen(props: any) {
+    // console.log(">>> check props", props);
+    const navigation = props.navigation;
     return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
-                <Stack.Screen name="Details" component={DetailsScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <View style={{ marginVertical: 10 }}>
+          <Button
+            title="go to details"
+            onPress={() => navigation.navigate('Details')}
+          />
+        </View>
+        <Text>Home Screen</Text>
+        <View style={{ marginVertical: 10 }}>
+          <Button
+            title="go to user id =1"
+            onPress={() => navigation.navigate('Details')}
+          />
+        </View>
+        <Text>Home Screen</Text>
+        <View style={{ marginVertical: 10 }}>
+          <Button
+            title="go to user id =2"
+            onPress={() => navigation.navigate('Details')}
+          />
+        </View>
+
+      </View>
     );
+  }
+
+
+  function DetailsScreen(props: any) {
+    const navigation: any = useNavigation();
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <Text>user id = ??</Text>
+        <Button title="go back home" onPress={() => navigation.goBack()}
+        />
+      </View>
+    );
+  }
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 
 }
